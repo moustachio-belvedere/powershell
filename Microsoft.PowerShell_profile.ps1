0e316b875732fcd22c5d19339e07c0ae4c5d9d09
Louis Kaplan <j.kiszle@gmail.com>
2021-01-15 23:50:35 +0000

    license added

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,31 @@
-function prompt {
-  $p = Split-Path -leaf -path (Get-Location)
-  if ($p.length -gt 3) {
-    $p = $p.Substring(0,3)
-  }
-  "$p> "
+function getbranch() {
+    try {
+        $branch = git rev-parse --abbrev-ref HEAD
+
+        if ($branch -eq "HEAD") {
+            # we're probably in detached HEAD state, so print the SHA
+            $branch = git rev-parse --short HEAD
+        }
+    } catch {
+        # we'll end up here if we're in a newly initiated git repo
+        $branch = "(no branches)"
+    }
+    return "($branch)"
 }
 
+function prompt {
+    $p = Split-Path -leaf -path (Get-Location)
+    if ($p.length -gt 3) {
+      $p = $p.Substring(0,3)
+    }
+
+    if (Test-Path .git) {
+        $branch = getbranch
+        $out = "$p $branch"
+    }
+    else {
+        $out = $p 
+    }
+
+    return "$out > "
+}
