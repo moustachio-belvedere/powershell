4d660fa9cdc23fc562ecd116f229a50cc610091a
Louis Kaplan <j.kiszle@gmail.com>
2021-01-16 01:02:35 +0000

    almost there

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,40 +1,49 @@
-function getbranch() {
+function putbranch {
+    Param($dir)
     # modified from:
     # https://stackoverflow.com/a/44411205/2665020
     try {
-        $branch = git rev-parse --abbrev-ref HEAD
+        $branch = git --git-dir $dir rev-parse --abbrev-ref HEAD
 
         if ($branch -eq "HEAD") {
             # we're probably in detached HEAD state, so print the SHA
-            $branch = git rev-parse --short HEAD
+            $branch = git --git-dir $dir rev-parse --short HEAD
         }
     } catch {
         # we'll end up here if we're in a newly initiated git repo
         $branch = "(no branches)"
     }
-    return " ($branch)"
+    Write-Host "($branch) " -NoNewLine
 }
 
-function findclosestgit() {
+function findclosestgit {
+    Param($loc)
     # function climbs dir structure
     # until it finds a git repo
-
+    $gitquery = Join-Path -Path $loc -ChildPath ".git"
+    if ($loc) {
+        if (Test-Path $gitquery) {
+            putbranch($gitquery)
+        }
+        else {
+            $par = Split-Path -Parent $loc
+            findclosestgit($par)
+        }
+    }
 }
 
-function prompt {
-    $p = Split-Path -leaf -path (Get-Location)
+function shorteneddir {
+    Param($loc)
+    $p = Split-Path -leaf -path $loc
     if ($p.length -gt 3) {
       $p = $p.Substring(0,3)
     }
-    Write-Host $p -NoNewLine
-        
-    if (Test-Path .git) {
-        $branch = getbranch
-        $out = "$p $branch"
-    }
-    else {
-        $out = $p 
-    }
+    Write-Host "$p " -NoNewLine
+}
 
-    return " > "
+function prompt {
+    $loc = (Get-Location)
+    shorteneddir($loc)
+    findclosestgit($loc)
+    return "> "
 }
